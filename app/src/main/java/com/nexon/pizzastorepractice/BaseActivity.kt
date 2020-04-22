7f02bf31987f84791c0595f4a1c60ba2ef64050f
RobertHan96 <yshan4329@gmail.com>
2020-04-22 21:07:12 +0900

    4th commit : 리스트뷰 어댑터 생성 : 리스트뷰와 어댑터 연결, 리스트뷰 관련 작업 완료 : 사용 라이브러리, 메니페스트 권한 추가

--- app/src/main/java/com/nexon/pizzastorepractice/BaseActivity.kt
+++ app/src/main/java/com/nexon/pizzastorepractice/BaseActivity.kt
@@ -3,6 +3,7 @@ package com.nexon.pizzastorepractice
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
+    val mContext = this
     abstract fun setupEvents()
     abstract fun setValues()
 }
--- app/src/main/java/com/nexon/pizzastorepractice/adapters/StoreAdapter.kt
+++ app/src/main/java/com/nexon/pizzastorepractice/adapters/StoreAdapter.kt
@@ -9,31 +9,31 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.nexon.pizzastorepractice.R
 import com.nexon.pizzastorepractice.datas.Store
+import com.bumptech.glide.Glide
+
 import org.w3c.dom.Text
 class StoreAdapter (context : Context, resID:Int, list:ArrayList<Store>)  : ArrayAdapter<Store>(context, resID, list)  {
     val mContext = context
     val mList = list
     val inf = LayoutInflater.from(mContext)
 
-// Swift에서 하나의 테이블뷰 아이템을 만드는 함수와 기능이 같음
-// getView는 indexpath.row와 유사함 => 각 줄에 뿌려줄 내용을 결정
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
-        if (tempRow == null) {
+        tempRow.let {
+
+        }.let {
             tempRow = inf.inflate(R.layout.store_list_item, null)
-//            미리 만들어놓은 레이아웃(XML)파일을 inf()함수를 거쳐서 내가 뿌려줄 리스트에 연결
         }
-//    위에서 이미 null 체크가 끝났으므로 내가 가져올 리스트 아이템은 !!를 사용해 강제 언랩핑해서 사용 준비
+
         val row = tempRow!!
-//    XML에 있는 뷰들을 변수로 담기, 해당 뷰들의 값들을 뿌려줄때 변경하기 위함
+        val data = mList.get(position)
+
         val storeName = row.findViewById<TextView>(R.id.storeNameText)
         val storeLogoImg = row.findViewById<ImageView>(R.id.storeLogoImg)
 
-        addressText.text = "${data.address}, ${floorStr}"
-        descText.text = data.desc
+        storeName.text = data.Name
+        Glide.with(mContext).load(data.logoUrl).into(storeLogoImg)
 
-
-//    최종적으로 화면에 뿌려질 하나의 리스트(row)를 최종적으로 return
         return  row
 
     }
