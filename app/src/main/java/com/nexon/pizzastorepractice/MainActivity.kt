4f5102acd9c40f00a9be3717ca2563fad4e86e6c
RobertHan96 <yshan4329@gmail.com>
2020-04-22 22:22:12 +0900

    7th commit : StoreDetail 액티비티 완성 : 전화 액션 인텐트 추가

--- app/src/main/java/com/nexon/pizzastorepractice/MainActivity.kt
+++ app/src/main/java/com/nexon/pizzastorepractice/MainActivity.kt
@@ -44,13 +44,13 @@ class MainActivity : BaseActivity() {
 
     fun fetchPizzaStores() {
         storeList.add(Store("https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg",
-            "피자헛","010-2234-5619"))
+            "피자헛","010-2186-5619"))
         storeList.add(Store("http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800",
-            "파파존스","010-2234-5619"))
+            "파파존스","010-1765-2159"))
         storeList.add(Store("https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200",
-            "미스터피자","010-2234-5619"))
+            "미스터피자","010-2234-1234"))
         storeList.add(Store("https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png",
-            "도미노피자","010-2234-5619"))
+            "도미노피자","010-9626-5962"))
     }
 
 
--- app/src/main/java/com/nexon/pizzastorepractice/StoreDetailActivity.kt
+++ app/src/main/java/com/nexon/pizzastorepractice/StoreDetailActivity.kt
@@ -1,8 +1,17 @@
 package com.nexon.pizzastorepractice
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.nexon.pizzastorepractice.datas.Store
+import kotlinx.android.synthetic.main.activity_store_detail.*
 
 class StoreDetailActivity : BaseActivity() {
     private lateinit var storeData : Store
@@ -15,11 +24,36 @@ class StoreDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        btnCall.setOnClickListener {
+            val permissionListener = object  : PermissionListener {
+                override fun onPermissionGranted() {
+                    val uri = Uri.parse("tel:01022345619")
+                    val myIntent = Intent(Intent.ACTION_CALL, uri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한 허용 필요", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext).setPermissionListener(permissionListener).setDeniedMessage("설정에서 권한을 허용해주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE).check()
+
+        }
     }
 
     override fun setValues() {
-        storeData = intent.getSerializableExtra("storeData") as Store
+        fetchStoreData()
+    }
 
+    fun fetchStoreData() {
+        storeData = intent.getSerializableExtra("storeData") as Store
+        storeName.text = storeData.Name
+        storePhone.text = storeData.phone
+        val storeDetailLogoImg = findViewById<ImageView>(R.id.storeDetailLogoImg)
+        Glide.with(mContext).load(storeData.logoUrl).into(storeDetailLogoImg)
     }
 
 }
